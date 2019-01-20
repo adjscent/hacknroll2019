0d6e64ab382ba97f9ec6084a8f974b8e8d1f184a
adjscent <jasonsohjsh@gmail.com>
2019-01-20 12:08:45 +0800

    Swap backup home page

--- delegat.io/src/routes/index.jsx
+++ delegat.io/src/routes/index.jsx
@@ -10,8 +10,8 @@ var indexRoutes = [
   // { path: "/call-page", name: "CallPage", component: CallPage },
   { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
   { path: "/login-page", name: "LoginPage", component: LoginPage },
-  { path: "/home", name: "MainPage", component: MainPage },
-  { path: "/home2", name: "UserPage", component: UserPage },
+  // { path: "/home", name: "MainPage", component: MainPage },
+  { path: "/home", name: "UserPage", component: UserPage },
   { path: "/", name: "LandingPage", component: LandingPage },
   { path: "*", component: Page404 }
 ];
