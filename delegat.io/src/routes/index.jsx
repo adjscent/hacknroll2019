1633cfa5a1810195fe9aa94526493c2ecfc8d88a
adjscent <jasonsohjsh@gmail.com>
2019-01-20 12:08:10 +0800

    Fix naming issues in router

--- delegat.io/src/routes/index.jsx
+++ delegat.io/src/routes/index.jsx
@@ -1,11 +1,13 @@
 import LandingPage from "views/Landing/LandingPage.jsx";
-import UserPage from "views/Landing/LandingPage.jsx";
+import UserPage from "views/Landing/UserPage.jsx";
 import MainPage from "views/Main/MainPage.jsx";
 import ProfilePage from "views/Login/ProfilePage.jsx";
 import LoginPage from "views/Login/LoginPage.jsx";
 import Page404 from "views/Others/Page404.jsx";
+// import CallPage from "views/HTML/CallPage.jsx";
 
 var indexRoutes = [
+  // { path: "/call-page", name: "CallPage", component: CallPage },
   { path: "/profile-page", name: "ProfilePage", component: ProfilePage },
   { path: "/login-page", name: "LoginPage", component: LoginPage },
   { path: "/home", name: "MainPage", component: MainPage },
